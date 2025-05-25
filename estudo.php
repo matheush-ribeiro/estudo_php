6388186a5fe76c068fc6cb031eeac444b63f091b
Matheus H. Ribeiro <matheush_ribeiro@outlook.com>
2025-05-25 14:00:07 -0300

    commit 2

--- estudo.php
+++ estudo.php
@@ -46,13 +46,98 @@ echo $resto;
 echo '<br>';
 
 //condições
-$variavel1 = 'Matheus';
+
+/*$variavel1 = 'Matheus';
 $variavel2 = 'Matheus';
 
 if ($variavel1 == $variavel2){
     echo '<div style="width:10px;height:10px;background:black;"></div';
+    echo '<br>';
 }
 else {
     echo 'A condição não bate';
+}*/
+
+$var1 = 13 + 7;
+$var2 = 13 + 7;
+
+if ($var1 == $var2) {
+    echo 'Variável 1 é igual a variável 2';
+}
+else if ($var1 > $var2) {
+    echo 'Variável 1 é igual ou maior que variável 2';
+}
+else {
+    echo 'Variável 1 é manor que variável 2';
+}
+echo '<br>';
+
+//operadores
+#um sinal de igual quer dizer que estamos atribuindo um valor, sempre retorna verdadeiro.
+$nome_01 = 'mah';
+$nome_02 = 'lah';
+
+if ($nome_01 = $nome_02) {
+    echo 'Verdade';
+}
+else {
+    echo 'Falso';
+}
+echo '<br>';
+#dois iguais comparam se os valores são iguais.
+$nome_001 = 'mah';
+$nome_002 = 'lah';
+
+if ($nome_001 == $nome_002) {
+    echo 'Verdade';
+}
+else {
+    echo 'Falso';
+}
+echo '<br>';
+#sinal de != conferimos para ver se é diferente.
+$nome_0001 = 'mah';
+$nome_0002 = 'lah';
+
+if ($nome_0001 != $nome_0002) {
+    echo 'Verdade';
+}
+else {
+    echo 'Falso';
+}
+echo '<br>';
+#três sinais de igual conferem se são identicos, ou seja mesmo valor e mesmo tipo.
+$nome_00001 = 'mah';
+$nome_00002 = 'lah';
+
+if ($nome_00001 === $nome_00002) {
+    echo 'Verdade';
+}
+else {
+    echo 'Falso';
+}
+echo '<br>';
+
+//operadores matemáticos e lógicos
+$numero_01 = 21;
+$numero_02 = 20;
+
+if ($numero_01 < $numero_02) {
+    echo 'Número 1 é menor que número 2';
+}
+else if ($numero_01 <= $numero_02) {
+    echo 'Número 1 é menor ou igual ao número 2';
+}
+else if ($numero_01 == $numero_02) {
+    echo 'Número 1 é igual ao número 2';
+}
+/*else if ($numero_01 >= $numero_02) {
+    echo 'Número 1 é maior ou igual ao número 2';
+}*/
+else if ($numero_01 > $numero_02) {
+    echo 'Número 1 é maior que número 2';
+}
+else {
+    echo 'Nenhuma das condições batem';
 }
 ?>
